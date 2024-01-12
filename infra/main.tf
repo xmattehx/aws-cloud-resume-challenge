9d1f9e36e76a6f0317533e82a8df92f239f7367f
Matthew Erbacher <merbacherdev@gmail.com>
2024-01-12 15:30:38 -0800

    Adding Terraform infra

--- infra/main.tf
+++ infra/main.tf
@@ -5,8 +5,23 @@
 resource "aws_instance" "testdeploy" {
     ami = "ami-05ed7c410d6798451"
     instance_type = "t2.micro"
+    key_name = "keypair"
+    subnet_id = aws_subnet.subnet1.id
+    vpc_security_group_ids = [aws_security_group.allow_rdp.id]
 
 tags = {
      Name = "WinServer_AnsibleTst"
+  }
+}
+
+resource "aws_instance" "ansiblecont" {
+    ami = "ami-0a2e7efb4257c0907"
+    instance_type = "t2.micro"
+    key_name = "keypair"
+    subnet_id = aws_subnet.subnet1.id
+    vpc_security_group_ids = [aws_security_group.allow_port_22.id]
+
+tags = {
+     Name = "Ansible_Controller"
   }
 }
---
+++ infra/networking.tf
@@ -0,0 +1,96 @@
+//Create VPC
+resource "aws_vpc" "main" {
+  cidr_block = "10.10.0.0/16"
+}
+
+//Create subnet
+resource "aws_subnet" "subnet1" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.10.1.0/24"
+
+  tags = {
+    Name = "subnet1"
+  }
+}
+
+//Create Internet Gateway
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "gw"
+  }
+}
+
+//Reoute Table
+resource "aws_route_table" "routetable" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gw.id
+  }
+
+  tags = {
+    Name = "RouteTable"
+  }
+}
+
+//Route Table Association
+resource "aws_route_table_association" "rt_a" {
+  subnet_id      = aws_subnet.subnet1.id
+  route_table_id = aws_route_table.routetable.id
+}
+
+resource "aws_security_group" "allow_rdp" {
+  name        = "allow_rdp"
+  description = "Allow rdp inbound traffic and all outbound traffic"
+  vpc_id      = aws_vpc.main.id
+
+  tags = {
+    Name = "allow_rdp"
+  }
+}
+
+//GetMyLocalIP
+data "http" "myip" {
+  url = "https://ipv4.icanhazip.com"
+}
+
+resource "aws_vpc_security_group_ingress_rule" "allow_rdp_ipv4" {
+  security_group_id = aws_security_group.allow_rdp.id
+  cidr_ipv4         = aws_vpc.main.cidr_block
+  from_port         = 3389
+  ip_protocol       = "tcp"
+  to_port           = 3389
+}
+
+resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4_port3389" {
+  security_group_id = aws_security_group.allow_rdp.id
+  cidr_ipv4         = "0.0.0.0/0"
+  ip_protocol       = "-1" # semantically equivalent to all ports
+}
+
+resource "aws_security_group" "allow_port_22" {
+  name        = "allow_port 22"
+  description = "Allow port 22 inbound traffic and all outbound traffic"
+  vpc_id      = aws_vpc.main.id
+
+  tags = {
+    Name = "allow_Port_22"
+  }
+}
+
+resource "aws_vpc_security_group_ingress_rule" "allow_22_ipv4" {
+  security_group_id = aws_security_group.allow_port_22.id
+  cidr_ipv4         = aws_vpc.main.cidr_block
+  from_port         = 22
+  ip_protocol       = "tcp"
+  to_port           = 22
+}
+
+resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4_port22" {
+  security_group_id = aws_security_group.allow_port_22.id
+  cidr_ipv4         = "0.0.0.0/0"
+  ip_protocol       = "-1" # semantically equivalent to all ports
+}
